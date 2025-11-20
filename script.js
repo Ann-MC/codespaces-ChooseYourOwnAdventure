23b564184350676658bdf03cbd4935bbe11e1781
Ann-MC <147668466+Ann-MC@users.noreply.github.com>
2025-11-20 15:26:29 +0000

    edited text

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let begPic = document.querySelector(".begPic");
 let setup = document.querySelector(".setup");
+let setMid = document.querySelector(".setMid");
 let imageOne = document.querySelector(".imageOne");
 let imageTwo = document.querySelector(".imageTwo");
 
@@ -25,7 +26,9 @@ optionOne.addEventListener("click", function () {
   imageTwo.src = "images/watchInHorror.jpg";
 
   begPic.src ="images/killer.jpg";
-
+  begPic.style.width="70%";
+  
+  
   optionOne.style.display = "none";
   optionTwo.style.display = "none";
 
@@ -40,11 +43,16 @@ optionOne.addEventListener("click", function () {
 
   setup.innerHTML = "You jump in the way of the blade, saving Duke Colten’s life. He defeats the assassin and you both live happily ever after…until you find out he’s having an affair with the saintess and planning a coup. He murders you in cold blood so you don’t tell anyone. Yeesh.";
   begPic.src ="images/flDead.png";
+
+  setMid.style.marginRight= "25%";
+  setMid.style.marginTop= "10%";
   
   restart.style.display = "block";
 
-  imageOne.style.display="none";
-  imageTwo.style.display="none";
+  // imageOne.style.display="none";
+  // imageTwo.style.display="none";
+  imageOne.style.visibility = "hidden";
+  imageTwo.style.visibility = "hidden";
 
   optionOneAA.style.display = "none";
   optionTwoAB.style.display = "none";
@@ -57,11 +65,15 @@ optionOne.addEventListener("click", function () {
   optionTwoAB.addEventListener("mouseover", function () {
   setup.innerHTML = "The assassin murders Duke Colten. It turns out he’s holding a coup against the monarchy. You go with him to tear down the corrupt system that has plagued this world. You join the revolution and succed in burning down the royals.";
   begPic.src ="images/burningKingdom.jpg";
+  begPic.style.width="100%";
+
+  setMid.style.marginRight= "12%";
+  setMid.style.marginTop= "10%";
 
   restart.style.display = "block";
 
-  imageOne.style.display="none";
-  imageTwo.style.display="none";
+  imageOne.style.visibility = "hidden";
+  imageTwo.style.visibility = "hidden";
 
   optionOneAA.style.display = "none";
   optionTwoAB.style.display = "none";
@@ -73,6 +85,9 @@ optionOne.addEventListener("click", function () {
 
 optionTwo.addEventListener("click", function () {
   setup.innerHTML = "As you both walk out of the palace, suddenly, your rival, Vivian Villaine, pops up out of nowhere and asks for you to meet with her.";
+
+  setMid.style.marginRight= "0%";
+  setMid.style.marginTop= "17%";
 
   optionOne.style.display = "none";
   optionTwo.style.display = "none";
@@ -90,13 +105,16 @@ optionTwo.addEventListener("click", function () {
 // route BA
 
   optionOneBA.addEventListener("mouseout", function () {
-  setup.innerHTML = "Turns out she’s really the disguised cold Duke of the north younger sister, and wants to get married and run away with you. You both get married and run off living happily ever after.";
+  setup.innerHTML = "Turns out she's been secretly in love with you the entire time, and wants to run away with you. You both get married and run off, living happily ever after.";
   begPic.src ="images/loveWinners.jpg";
+
+  setMid.style.marginRight= "8%";
+  setMid.style.marginTop= "15%";
 
   restart.style.display = "initial";
 
-  imageOne.style.display = "none";
-  imageTwo.style.display = "none";
+  imageOne.style.visibility = "hidden";
+  imageTwo.style.visibility = "hidden";
 
 
   optionOneBA.style.display = "none";
@@ -112,10 +130,15 @@ optionTwoBB.addEventListener("mouseup", function () {
   setup.innerHTML = "You and Prince Anderson live happily ever after…at least until the civil war began. After his brother died, leaving him the throne, Anderson started pulling away more and more, causing you to complain. In order to keep you around he proposes and so you keep your head you accept. Now your getting married, so happily ever after … i guess.";
   begPic.src ="images/sadMarriage.jpg";
 
+  begPic.style.width="70%"
+
+  setMid.style.marginRight= "37%";
+  setMid.style.marginTop= "5%";
+
   restart.style.display = "initial";
 
-  imageOne.style.display = "none";
-  imageTwo.style.display = "none";
+  imageOne.style.visibility = "hidden";
+  imageTwo.style.visibility = "hidden";
 
 
   optionOneBA.style.display = "none";
@@ -132,12 +155,16 @@ restart.addEventListener("click", function () {
   
   restart.style.display = "none";
 
-  imageOne.style.display = "initial";
-  imageTwo.style.display = "initial";
+  imageOne.style.visibility = "initial";
+  imageTwo.style.visibility = "initial";
+
+  setMid.style.marginRight = "10%";
+  setMid.style.marginTop = "35%";
 
   imageOne.src = "images/dukey.jpg";
   imageTwo.src = "images/prince.jpg";
 
   begPic.src ="images/fl.png";
+  begPic.style.width="90%";
 
 });
